600a2acaf0c396c64a61e8605035368113a4ad5f
jsp <jspenkar@gmail.com>
2015-04-29 01:00:24 -0500

    Maps are mounting.

--- js/bundle.js
+++ js/bundle.js
@@ -1,17 +1,20 @@
-var creatMap = function(id) {
-	var mapOptions = {
-		zoom: 8,
-		center: new google.maps.LatLng(-34.397, 150.644)
-	};
-	map = new google.maps.Map(id, mapOptions);
-}
+// var creatMap = function(id) {
+// 	var mapOptions = {
+// 		zoom: 8,
+// 		center: new google.maps.LatLng(-34.397, 150.644)
+// 	};
+// 	map = new google.maps.Map(id, mapOptions);
+// }
 
 var GoogleMap = React.createClass({
 	displayName:'GoogleMap',
+	getInitialState: function(){
+		return ({title:'stuff'})
+	},
 	render: function(){
 		return React.createElement('div', {className: 'maps'},
-			React.createElement('div', {id:'map2'}),
-			React.createElement('div', {id:'map3'})
+			React.createElement('div', {id:'mapA', className:'map'}),
+			React.createElement('div', {id:'mapB', className:'map'})
 		)
 	},
 	componentDidMount: function(){
@@ -20,23 +23,23 @@ var GoogleMap = React.createClass({
 			zoom: 8,
 			center: new google.maps.LatLng(-34.397, 150.644)
 		};
-		new google.maps.Map(document.getElementById('map2'), mapOptions);
-		// new google.maps.Map(document.getElementById('map3'), mapOptions);
+		new google.maps.Map(document.getElementById('mapA'), mapOptions);
+		new google.maps.Map(document.getElementById('mapB'), mapOptions);
 	}
 })
 
-React.render(
-	React.createElement(GoogleMap, null),
-	document.getElementById('map2')
-);
 
 function initialize() {
-  var mapOptions = {
-    zoom: 8,
-    center: new google.maps.LatLng(-34.397, 150.644)
-  };
-  new google.maps.Map(document.getElementById('map'),
-      mapOptions);
+	var mapOptions = {
+		zoom: 8,
+		center: new google.maps.LatLng(-34.397, 150.644)
+	};
+	new google.maps.Map(document.getElementById('map'), mapOptions);
+
+	React.render(
+		React.createElement(GoogleMap, null),
+		document.getElementById('map2')
+	);
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
